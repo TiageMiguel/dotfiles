2ffa06e9f8a591a240ab1ef09c90f1eb3e3d1629
Tiago Miguel <tiagompmiguel@gmail.com>
2026-03-27 23:53:56 +0000

    feat: add zsh-syntax-highlighting and zsh-autosuggestions to Brewfile.

--- install.zsh
+++ install.zsh
@@ -5,3 +5,6 @@ find ./scripts -type f \( -name "*.zsh" -o -name "*.sh" \) -exec chmod +x {} \;
 
 echo "(.dotfiles) Running homebrew script..."
 zsh ./scripts/homebrew.zsh
+
+# Install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
